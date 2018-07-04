a3803bcf521e9e596b7d11ad74e4e8c1e395f528
muhdazmitomirin <muhdazmitomirin@gmail.com>
2018-07-04 12:04:47 +0800

    Add login,logout & reset password form template

--- student/views.py
+++ student/views.py
@@ -12,22 +12,26 @@ from django.db.models import Count, Sum, Q, Case, Value, When, IntegerField
 
 # Create your views here.
 
+@login_required(login_url='/accounts/login/')
 def home(request):
 	return render(request,'base.html')
 
+@login_required(login_url='/accounts/login/')
 def student_detail(request,pk):
     student = get_object_or_404(Student, pk=pk)
     print(student)
     print(student.name)
     return render(request, 'student/student_detail.html', {'student': student})
 
-
+@login_required(login_url='/accounts/login/')
 def home_sbadmin(request):
     return render(request, 'student/index.html') 
 
+@login_required(login_url='/accounts/login/')
 def home_json(request):
     return render(request, 'student/home_json.html')   
 
+@login_required(login_url='/accounts/login/')
 def student_new(request):
 
     if request.method == "POST":
@@ -43,6 +47,7 @@ def student_new(request):
     print(request.user)
     return render(request, 'student/student_new.html', {'form': form})
 
+@login_required(login_url='/accounts/login/')
 def student_edit(request,pk):
 
     student = get_object_or_404(Student, pk=pk)
@@ -60,7 +65,7 @@ def student_edit(request,pk):
     
     return render(request, 'student/student_edit.html', {'form': form})
 
-
+@login_required(login_url='/accounts/login/')
 def student_remove(request,pk):
 
     student = get_object_or_404(Student, pk=pk)
